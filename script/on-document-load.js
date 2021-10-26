5024c00f3cf5387ddd8900a4ad4adb6835483336
canderson <canderson@yext.com>
2021-10-26 15:14:28 +0000

    Use token from window

---
+++ script/on-document-load.js
@@ -0,0 +1,9 @@
+/* Code added here will be executed on the DOMContentLoaded event */
+
+
+if (!window.YEXT_TOKENS || !window.YEXT_TOKENS.token1 || !window.YEXT_TOKENS.token1.token) {
+  console.log('The auth token was not found on the window at window.YEXT_TOKENS.token1.token');
+} else {
+  const token = window.YEXT_TOKENS.token1.token;
+  AnswersExperience.runtimeConfig.set('tokenHeader', token);
+}
