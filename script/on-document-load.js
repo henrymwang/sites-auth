022c4abcf00b5c976accd1275bf9de47ada6dcf1
canderson <canderson@yext.com>
2021-10-26 15:18:38 +0000

    print token

--- script/on-document-load.js
+++ script/on-document-load.js
@@ -5,5 +5,6 @@ if (!window.YEXT_TOKENS || !window.YEXT_TOKENS.token1 || !window.YEXT_TOKENS.tok
   console.log('The auth token was not found on the window at window.YEXT_TOKENS.token1.token');
 } else {
   const token = window.YEXT_TOKENS.token1.token;
+  console.log(`Setting token ${token} in the runtime config`);
   AnswersExperience.runtimeConfig.set('tokenHeader', token);
 }
