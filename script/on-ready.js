44b49e3b600b374469893c28059731da7ef1d9cb
canderson <canderson@yext.com>
2021-10-27 19:59:40 +0000

    point to sandbox

---
+++ script/on-ready.js
@@ -0,0 +1,7 @@
+/* Code added here will be executed during the Answers onReady callback */
+
+ANSWERS.core._coreLibrary.searchService.universalSearchEndpoint = "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query"
+ANSWERS.core._coreLibrary.searchService.verticalSearchEndpoint = "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query"
+ANSWERS.core._coreLibrary.autoCompleteService.universalEndpoint = "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete"
+ANSWERS.core._coreLibrary.autoCompleteService.verticalEndpoint = "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete"
+ANSWERS.core._coreLibrary.questionSubmissionService.endpoint = "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion"
